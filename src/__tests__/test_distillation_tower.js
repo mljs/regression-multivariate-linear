db27acc427ce288d1d318521e1bb24dc69dfd0b7
Océane Patiny <oceane@patiny.com>
2022-06-21 14:10:05 +0200

    refactor: cleanup
    Refs: mljs/ml#174

--- src/__tests__/test_distillation_tower.js
+++ src/__tests__/test_distillation_tower.js
@@ -1,24 +1,24 @@
-import MLR from '..';
+import MLR from "..";
 
-const fs = require('fs');
-const { join } = require('path');
+const fs = require("fs");
+const { join } = require("path");
 
-const Papa = require('papaparse');
+const Papa = require("papaparse");
 
 // Dataset -> https://openmv.net/info/distillation-tower
 
-describe('multivariate linear regression', () => {
-  it('should work with large dataset with intercept', () => {
+describe("multivariate linear regression", () => {
+  it("should work with large dataset with intercept", () => {
     const ymatrix = fs.readFileSync(
-      join(__dirname, '../../data/ydistillationjs.csv'),
-      'utf8',
+      join(__dirname, "../../data/ydistillationjs.csv"),
+      "utf8"
     );
-    const y = Papa.parse(ymatrix, { delimiter: ',', dynamicTyping: true }).data;
+    const y = Papa.parse(ymatrix, { delimiter: ",", dynamicTyping: true }).data;
     const xmatrix = fs.readFileSync(
-      join(__dirname, '../../data/xdistillationjs.csv'),
-      'utf8',
+      join(__dirname, "../../data/xdistillationjs.csv"),
+      "utf8"
     );
-    const x = Papa.parse(xmatrix, { delimiter: ',', dynamicTyping: true }).data;
+    const x = Papa.parse(xmatrix, { delimiter: ",", dynamicTyping: true }).data;
     //
     const mlr = new MLR(x, y, { intercept: true });
     expect(mlr.predict(x[0]).map(Math.round)).toStrictEqual([33]);
@@ -30,17 +30,17 @@ describe('multivariate linear regression', () => {
     expect(mlr.predict(x[60]).map(Math.round)).toStrictEqual([35]);
     expect(mlr.predict(x[250]).map(Math.round)).toStrictEqual([36]);
   });
-  it('should work with large dataset without intercept', () => {
+  it("should work with large dataset without intercept", () => {
     const ymatrix = fs.readFileSync(
-      join(__dirname, '../../data/ydistillationjs.csv'),
-      'utf8',
+      join(__dirname, "../../data/ydistillationjs.csv"),
+      "utf8"
     );
-    const y = Papa.parse(ymatrix, { delimiter: ',', dynamicTyping: true }).data;
+    const y = Papa.parse(ymatrix, { delimiter: ",", dynamicTyping: true }).data;
     const xmatrix = fs.readFileSync(
-      join(__dirname, '../../data/xdistillationjs.csv'),
-      'utf8',
+      join(__dirname, "../../data/xdistillationjs.csv"),
+      "utf8"
     );
-    const x = Papa.parse(xmatrix, { delimiter: ',', dynamicTyping: true }).data;
+    const x = Papa.parse(xmatrix, { delimiter: ",", dynamicTyping: true }).data;
     //
     let mlr = new MLR(x, y, { intercept: false });
     expect(mlr.predict(x[0]).map(Math.round)).toStrictEqual([33]);
